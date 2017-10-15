b31af3ff748b634439639cff058ed6845fd406c3
Cyrus Vandrevala <cyrus.vandrevala@gmail.com>
2017-10-14 19:08:55 -0500

    Touch up formatting on balance sheet

--- scripts/balance_sheet.py
+++ scripts/balance_sheet.py
@@ -7,15 +7,17 @@ from utilities.constants import Constants
 portfolio = PortfolioCreator(Constants.LOCAL_LEDGER_PATH).create()
 data = []
 
-data.append(["Last Updated", "Institution", "Name", "Owner", "Asset Class", "Value"])
+data.append(["Last Updated", "Institution", "Name", "Symbol", "Owner", "Asset Class", "Value"])
 
 for asset in portfolio.assets():
-    data.append([asset.last_updated(), asset.institution, asset.name, asset.owner, asset.asset_class(), str(asset.value())])
+    data.append([asset.last_updated(), asset.institution, asset.name, asset.symbol, asset.owner, asset.asset_class(), str(asset.value())])
+
+data.append(["---", "---", "---", "---", "---", "---"])
 
 for liability in portfolio.liabilities():
-    data.append([liability.last_updated(), liability.institution, liability.name, "-", "-", str(liability.value())])
+    data.append([liability.last_updated(), liability.institution, liability.name, liability.symbol, liability.owner, liability.asset_class(), str(-liability.value())])
 
-data.append(["", "", "", "", "Total", str(portfolio.total_value())])
+data.append(["", "", "", "", "", "Total", str(portfolio.total_value())])
 
 t = AsciiTable(data)
 t.inner_footing_row_border = True
