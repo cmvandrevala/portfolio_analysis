acd19747466fdb0d9f4b1f3091e9fe863fe9b05d
Cyrus Vandrevala <cyrus.vandrevala@gmail.com>
2017-10-25 16:54:11 -0500

    Add new asset classes

--- utilities/constants.py
+++ utilities/constants.py
@@ -1,7 +1,7 @@
 class Constants:
     DAYS_PER_YEAR = 365
     GENERAL_LEDGER_HEADERS = ["Timestamp", "Institution", "Description", "Owner", "Symbol", "Account Type", "Value", "Asset Class"]
-    LEDGERS_DIRECTORY = "../general_ledger/"
+    LEDGERS_DIRECTORY = "../general_ledger/csv/"
     LOCAL_LEDGER_PATH = "local_ledger.csv"
     SECONDS_PER_DAY = 86400
     YES_REPONSES = ["y", "ye", "yes"]
--- valid_options/asset_class.py
+++ valid_options/asset_class.py
@@ -1,9 +1,11 @@
 from enum import Enum
 
 class AssetClass(Enum):
+    ANNUITY = "Annuity"
     CASH_EQUIVALENTS = "Cash Equivalents"
     COMMODITIES = "Commodities"
     EQUITIES = "Equities"
+    FIXED_ASSETS = "Fixed Assets"
     FIXED_INCOME = "Fixed Income"
     REAL_ESTATE = "Real Estate"
     NONE = "None"
