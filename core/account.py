f0d0c5712b9e660dea2ffaf33da6c94bb3f59cf4
Cyrus Vandrevala <cyrus.vandrevala@gmail.com>
2017-11-07 21:51:34 -0600

    Color dates older than one week on jupyter notebook

--- core/account.py
+++ core/account.py
@@ -3,7 +3,6 @@ from core.snapshot_history import SnapshotHistory
 from utilities.constants import Constants
 from utilities.epoch_timestamp_converter import EpochTimestampConverter
 
-
 class Account:
     def __init__(self, name, owner, symbol, asset_class, institution, account_type):
         self.name = name
@@ -39,22 +38,36 @@ class Account:
                 self.account_type() == account.account_type())
 
     def balance_sheet_row(self):
-        colored_date = self.__unicode_color() + self.last_updated() + "\x1b[0m"
-        return [colored_date, self.institution, self.name, self.symbol, self.owner, self.asset_class(),
-                str(self.value())]
+        return [self.__last_updated_for_balance_sheet(), self.institution, self.name, self.symbol, self.owner, self.asset_class(), str(self.value())]
 
     def liabilities_row(self):
-        return [self.last_updated(), self.institution, self.name, self.owner, str(self.value())]
+        return [self.__last_updated_for_liabilities_row(), self.institution, self.name, self.owner, str(self.value())]
 
-    def __unicode_color(self):
+    def __last_updated_for_balance_sheet(self):
         last_updated_epoch = EpochTimestampConverter().epoch(self.last_updated())
         if last_updated_epoch > EpochTimestampConverter().epoch():
-            return "\x1b[1;31;40m"
-        elif last_updated_epoch < EpochTimestampConverter().epoch() - 90 * Constants.SECONDS_PER_DAY:
-            return "\x1b[1;31;40m"
-        elif last_updated_epoch < EpochTimestampConverter().epoch() - 60 * Constants.SECONDS_PER_DAY:
-            return "\x1b[1;35;40m"
-        elif last_updated_epoch < EpochTimestampConverter().epoch() - 30 * Constants.SECONDS_PER_DAY:
-            return "\x1b[0;33;40m"
+            return self.__color_last_updated("\x1b[1;31;40m")
+        elif self.__within_time_period(last_updated_epoch, 90):
+            return self.__color_last_updated("\x1b[1;31;40m")
+        elif self.__within_time_period(last_updated_epoch, 60):
+            return self.__color_last_updated("\x1b[1;35;40m")
+        elif self.__within_time_period(last_updated_epoch, 30):
+            return self.__color_last_updated("\x1b[0;33;40m")
         else:
-            return "\x1b[0;37;40m"
+            return self.__color_last_updated("\x1b[0;37;40m")
+
+    def __last_updated_for_liabilities_row(self):
+        last_updated_epoch = EpochTimestampConverter().epoch(self.last_updated())
+        if self.__within_time_period(last_updated_epoch, 7):
+            return self.__color_last_updated("\x1b[0;30;41m")
+        else:
+            return self.__color_last_updated()
+
+    def __within_time_period(self, last_updated_epoch, days):
+        return last_updated_epoch < EpochTimestampConverter().epoch() - days * Constants.SECONDS_PER_DAY
+
+    def __color_last_updated(self, color=None):
+        if color == None:
+            return self.last_updated()
+        else:
+            return color + self.last_updated() + "\x1b[0m"
--- tests/core/test_account.py
+++ tests/core/test_account.py
@@ -157,13 +157,21 @@ class AssetTestCase(unittest.TestCase):
         balance_sheet_row = self.account.balance_sheet_row()
         self.assertEqual(balance_sheet_row, ["\x1b[1;31;40m" + expected_date + "\x1b[0m", "Rachel's Bank","account name","SYMBOL","Bob Bobberson","Cash Equivalents","0"])
 
-    def test_it_returns_a_row_for_a_liability(self):
+    def test_it_returns_a_row_for_a_liability_less_than_one_week_old(self):
         date_difference = Constants.SECONDS_PER_DAY*2
         timestamp = EpochTimestampConverter().epoch()
         expected_date = EpochTimestampConverter().timestamp(timestamp - date_difference)
         self.liability.import_snapshot(timestamp - date_difference, 100)
         liabilities_row = self.liability.liabilities_row()
         self.assertEqual(liabilities_row, [expected_date, "Rachel's Bank","account name","Bob Bobberson","100"])
+
+    def test_it_returns_a_row_for_a_liability_greater_than_one_week_old(self):
+        date_difference = Constants.SECONDS_PER_DAY*9
+        timestamp = EpochTimestampConverter().epoch()
+        expected_date = EpochTimestampConverter().timestamp(timestamp - date_difference)
+        self.liability.import_snapshot(timestamp - date_difference, 100)
+        liabilities_row = self.liability.liabilities_row()
+        self.assertEqual(liabilities_row, ["\x1b[0;30;41m" + expected_date + "\x1b[0m", "Rachel's Bank","account name","Bob Bobberson","100"])
 
 if __name__ == '__main__':
     unittest.main()
