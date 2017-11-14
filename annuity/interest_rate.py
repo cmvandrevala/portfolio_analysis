7629eef2b5689e82077187dbb48c2e7ba75b94fb
Cyrus Vandrevala <cyrus.vandrevala@gmail.com>
2017-11-14 07:15:54 -0600

    Add calculation for per period interest rate

--- annuity/interest_rate.py
+++ annuity/interest_rate.py
@@ -1,10 +1,14 @@
 class InterestRate:
 
-    def __init__(self, nominal_interest_rate):
-        self.__nominal_interest_rate = nominal_interest_rate
+    def __init__(self, nominal_yearly_interest_rate):
+        self.__nominal_yearly_interest_rate = nominal_yearly_interest_rate
 
-    def nominal(self):
-        return self.__nominal_interest_rate
+    def nominal(self, periods=1):
+        return self.__per_period_rate(self.__nominal_yearly_interest_rate, periods)
 
-    def real(self, inflation_rate):
-        return (1 + self.__nominal_interest_rate) / (1 + inflation_rate) - 1
+    def real(self, inflation_rate, periods=1):
+        real_rate = (1 + self.__nominal_yearly_interest_rate) / (1 + inflation_rate) - 1
+        return self.__per_period_rate(real_rate, periods)
+
+    def __per_period_rate(self, rate, periods):
+        return (1 + rate)**(1/periods) - 1
--- tests/annuity/test_interest_rate.py
+++ tests/annuity/test_interest_rate.py
@@ -9,13 +9,19 @@ class InterestRateTestCase(unittest.TestCase):
         self.rate = InterestRate(0.07)
 
     def test_it_has_a_nominal_rate_of_interest(self):
-        self.assertEqual(self.rate.nominal(), 0.07)
+        self.assertAlmostEqual(self.rate.nominal(), 0.07, delta=0.001)
+
+    def test_it_returns_a_monthly_nominal_rate_of_interest(self):
+        self.assertAlmostEqual(self.rate.nominal(12), 0.0057, delta=0.001)
 
     def test_the_real_rate_of_interest_equals_the_nominal_rate_if_inflation_is_zero(self):
         self.assertAlmostEqual(self.rate.real(0.0), 0.07, delta = 0.001)
 
     def test_the_real_rate_of_interest_decreases_with_nonzero_inflation(self):
         self.assertAlmostEqual(self.rate.real(0.01), 0.06, delta=0.001)
+
+    def test_it_returns_a_daily_real_rate_of_interest(self):
+        self.assertAlmostEqual(self.rate.real(0.01, 365), 0.00016, delta=0.001)
 
 if __name__ == '__main__':
     unittest.main()
