277c5966b2b6c675eb5ed2c72b80bb8c568abdba
Cyrus Vandrevala <cyrus.vandrevala@gmail.com>
2018-03-03 21:35:32 -0600

    Refactor chart to use D3

--- app/static/net_worth_vs_time.js
+++ app/static/net_worth_vs_time.js
@@ -1,11 +1,56 @@
-var ctx = document.getElementById("net-worth-vs-time");
+var urlParams = new URLSearchParams(window.location.search);
+var start = urlParams.get('start')
+var end = urlParams.get('end')
 
-$.get( "/net_worth?start=2018-01-01&end=2018-02-02", function( data ) {
-  new tauCharts.Chart({
-      data: data,
-      type: 'line',
-      x: 'date',
-      y: 'value',
-      color: 'net-worth'
-  }).renderTo(ctx);;
+var margin = {top: 30, right: 30, bottom: 100, left: 100};
+var width = 1000 - margin.left - margin.right;
+var height = 500 - margin.top - margin.bottom;
+
+var parseTime = d3.timeParse("%Y-%m-%d");
+
+var x = d3.scaleTime().range([0, width]);
+var y = d3.scaleLinear().range([height, 0]);
+
+var line = d3.line()
+    .x(function(d) { return x(d.date); })
+    .y(function(d) { return y(d.value); });
+
+var svg = d3.select(".container")
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
+
+d3.json("/net_worth?start=" + start + "&end=" + end, function(error, data) {
+  if (error) throw error;
+
+  data.forEach(function(d) {
+      d.date = parseTime(d.date);
+      d.value = +d.value;
+  });
+
+  x.domain(d3.extent(data, function(d) { return d.date; }));
+  y.domain([0, d3.max(data, function(d) { return d.value; })]);
+
+  svg.append("path")
+      .data([data])
+      .attr("class", "line")
+      .attr("d", line);
+
+  svg.append("g")
+      .attr("class", "axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x)
+      .tickFormat(d3.timeFormat("%Y-%m-%d")))
+      .selectAll("text")
+      .style("text-anchor", "end")
+      .attr("dx", "-.8em")
+      .attr("dy", ".15em")
+      .attr("transform", "rotate(-65)");
+
+  svg.append("g")
+      .attr("class", "axis")
+      .call(d3.axisLeft(y));
 });
